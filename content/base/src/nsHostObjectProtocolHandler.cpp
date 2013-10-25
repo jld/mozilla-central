a721f8f8fcd3a0a8969aaf9ad4282aab4186834b
Jed Davis <jld@mozilla.com>
2013-10-24 17:07:04 -0700

    ...and add the principal

--- content/base/src/nsHostObjectProtocolHandler.cpp
+++ content/base/src/nsHostObjectProtocolHandler.cpp
@@ -82,13 +82,25 @@ class BlobURLsReporter MOZ_FINAL : public nsIMemoryReporter
                                    reinterpret_cast<void**>(&blob));
     if (blob) {
       nsAutoCString path, url;
+      char *origin;
       uint64_t size;
 
       if (NS_FAILED(blob->GetSize(&size))) {
         size = 0;
       }
 
-      path = "blob-urls/0x";
+      path = "blob-urls/";
+      if (NS_SUCCEEDED(aInfo->mPrincipal->GetOrigin(&origin))
+          && origin[0] != '\0') {
+        nsAutoCString escapedOrigin;
+
+        escapedOrigin.Assign(origin);
+        escapedOrigin.ReplaceChar('/', '\\');
+        path += escapedOrigin;
+      } else {
+        path += "unknown origin";
+      }
+      path += "/0x";
       path.AppendInt(reinterpret_cast<uintptr_t>(blob), 16);
       path += " ";
       url = aKey;
